2e2a2226cf00628d5e9f76b778a6dc4533ae5317
nathangappy <nate.gappy@gmail.com>
2020-01-21 00:54:42 -0500

    adding additional searchbar info

--- src/components/searchbar/searchbar.component.jsx
+++ src/components/searchbar/searchbar.component.jsx
@@ -16,12 +16,24 @@ const SearchBar = props => {
     <div className='searchbar'>
       <form action='' onSubmit={props.handleTickerSearch} className='tickerSearch'>
         <label htmlFor='tickerSearch'>Get Financial Data</label>
-        <input type='text' onChange={props.handleSearchChange} value={props.tickerSearch} name='tickerSearch' />
+        <input
+          type='text'
+          onChange={props.handleSearchChange}
+          value={props.tickerSearch}
+          name='tickerSearch'
+          placeholder='Enter Company Ticker'
+        />
         <button>Get Info</button>
       </form>
       <form action='' onSubmit={props.handleCompanyLookup} className='companySearch'>
         <label htmlFor='companySearch'>Search for Company Symbol</label>
-        <input type='text' onChange={props.handleSearchChange} value={props.companySearch} name='companySearch' />
+        <input
+          type='text'
+          onChange={props.handleSearchChange}
+          value={props.companySearch}
+          name='companySearch'
+          placeholder='Enter Company Name'
+        />
         <button>Find Symbol</button>
       </form>
       <ul>{companyLookupMatches}</ul>
