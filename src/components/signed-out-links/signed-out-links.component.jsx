96da23fc9fe70e59f393d3870808c004bae19415
nathangappy <nate.gappy@gmail.com>
2020-02-14 13:25:17 -0500

    changed navigation links

--- src/components/signed-out-links/signed-out-links.component.jsx
+++ src/components/signed-out-links/signed-out-links.component.jsx
@@ -18,16 +18,6 @@ const SignedOutLinks = ({ mobileMenu, handleMobileClick }) => {
         </Link>
       </li>
       <li onClick={handleMobileClick}>
-        <Link to='/signin' className='navLink'>
-          Sign In
-        </Link>
-      </li>
-      <li onClick={handleMobileClick}>
-        <Link to='/signup' className='navLink'>
-          Sign Up
-        </Link>
-      </li>
-      <li onClick={handleMobileClick}>
         <i className='fas fa-minus-circle' onClick={handleMobileClick}></i>
       </li>
     </ul>
